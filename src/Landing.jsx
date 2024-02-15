f5a4afcd963d8f11343d9e0b4ff6adf979b0c6fd
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-02-15 17:56:31 +0330

    Nav and Main part designed

---
+++ src/Landing.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./styles/styles.css";
+
+const Landing = () => {
+  return (
+    <div className="body">
+      <header>
+        <nav className="navbar">
+          <div>
+            <h1>Ehsan.</h1>
+          </div>
+          <div className="links">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#education">Education</a>
+            <a href="#skills">Skills</a>
+            <a href="#contact">Contact</a>
+          </div>
+        </nav>
+      </header>
+      <section className="main">
+        <div className="intro">
+        <h1>
+          Hi,I'm <span>Ehsan Fathalipour</span>
+        </h1>
+        </div>
+        <div className="role">
+          <h1>Front-End Developer</h1>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et ratione
+          inventore similique veniam, nemo minima eos magni? Animi quis a illo
+          quod, incidunt beatae repudiandae, adipisci neque labore magni
+          eveniet, voluptates at praesentium deleniti quaerat repellat. Quis
+          unde, nostrum reiciendis dolorem quia animi voluptatibus explicabo
+          assumenda inventore, excepturi, enim modi!
+        </p>
+        <div className="buttonContainer">
+          <button className="button1">Hire me</button>
+          <button className="button2">Lets Talk</button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Landing from './Landing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Landing />
   </React.StrictMode>
 );
 
