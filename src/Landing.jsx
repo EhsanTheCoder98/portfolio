545877fa797d316404327492776872d810bf60ef
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-02-16 13:26:16 +0330

    About me component design Done!

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,12 +4,14 @@ import "./styles/styles.css";
 // components
 import Header from "./components/Header";
 import Main from "./components/Main";
+import About from "./components/About";
 
 const Landing = () => {
   return (
     <div className="body">
       <Header />
       <Main />
+      <About />
     </div>
   );
 };
