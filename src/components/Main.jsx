61dff0081aa4388fe9af1d5ae42388259e52cf6a
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-02-16 12:21:20 +0330

    Main and Heade components built

---
+++ src/components/Main.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+// Icons
+import { FiGithub } from "react-icons/fi";
+import { TfiTwitter } from "react-icons/tfi";
+import { FiLinkedin } from "react-icons/fi";
+import { LuInstagram } from "react-icons/lu";
+import { LiaTelegram } from "react-icons/lia";
+
+const Main = () => {
+  return (
+    <div>
+        <section className="main">
+          <div className="intro">
+            <h1>
+              Hi,I'm <span>Ehsan Fathalipour</span>
+            </h1>
+          </div>
+          <div className="role">
+            <h1>Front-End Developer</h1>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et ratione
+            inventore similique veniam, nemo minima eos magni? Animi quis a illo
+            quod, incidunt beatae repudiandae, adipisci neque labore magni
+            eveniet, voluptates at praesentium deleniti quaerat repellat. Quis
+            unde, nostrum reiciendis dolorem quia animi voluptatibus explicabo
+            assumenda inventore, excepturi, enim modi!
+          </p>
+          <div className="buttonContainer">
+            <button className="button1">Hire me</button>
+            <button className="button2">Lets Talk</button>
+          </div>
+          <div className="icons">
+            <FiGithub className="github" />
+            <TfiTwitter className="github" />
+            <FiLinkedin className="github" />
+            <LuInstagram className="github" />
+            <LiaTelegram className="github" />
+          </div>
+        </section>
+      </div>
+  );
+};
+
+export default Main;
