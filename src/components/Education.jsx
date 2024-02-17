b0703ffe1ddce292953643de3d63b29719cb6965
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-02-17 19:24:45 +0330

    Education component design done!

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
 import "../styles/styles.css";
+import { SlCalender } from "react-icons/sl";
 
 const Education = () => {
-    return (
-        <div className='educationContainer'>
-            
+  return (
+    <div className="educationContainer">
+      <h1>
+        My <span>Journey!</span>
+      </h1>
+      <div className="eachJourneyDiv">
+        <div>
+          <div className="header">
+            <SlCalender className="icon" />
+            <span>September 2013 - May 2017</span>
+          </div>
+          <p>High School Diploma,Shahid Rajaee,Karaj-Iran</p>
         </div>
-    );
+        <div>
+          <div className="header">
+            <SlCalender className="icon" />
+            <span>September 2018 – May 2020</span>
+          </div>
+          <p>Management student at Azad University of Karaj (Resigned)</p>
+        </div>
+        <div>
+          <div className="header">
+            <SlCalender className="icon" />
+            <span>2010 – 2015</span>
+          </div>
+          <p>5 years of private English courses</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Education;
