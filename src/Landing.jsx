2b65f3c5b34e17f6ed1d54b4c34e2c0e259e16ce
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-02-20 17:28:18 +0330

    linked the nav links to components

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./styles/styles.css";
 import { FaAngleDoubleUp } from "react-icons/fa";
+import { Link } from 'react-scroll';
 
 // components
-import Header from "./components/Header";
 import Main from "./components/Main";
 import About from "./components/About";
 import Education from "./components/Education";
@@ -17,13 +17,24 @@ const Landing = () => {
       behavior: "smooth",
     });
   };
+  
   return (
     <div className="body">
-      <Header />
-      <Main />
-      <About />
-      <Education />
-      <Skills />
+        <nav className="navbar">
+          <div>
+            <h1>Ehsan.</h1>
+          </div>
+          <div className="links">
+            <Link to="main" spy={true} smooth={true} duration={100} offset={-70}>Home</Link>
+            <Link to="aboutContainer" spy={true} smooth={true} duration={100} offset={-40}>About</Link>
+            <Link to="educationContainer" spy={true} smooth={true} duration={100} offset={-40}>Education</Link>
+            <Link to="skills" spy={true} smooth={true} duration={100} offset={-250}>Skills</Link>
+          </div>
+        </nav>
+      <Main id="main" />
+      <About id="aboutContainer" />
+      <Education id="educationContainer" />
+      <Skills id="skills" />
       <Footer />
       <div className="flashUp">
         <FaAngleDoubleUp className="pointer" onClick={scrollToTop} />
