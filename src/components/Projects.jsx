e71e1234623b3528667323bdfcdb9fa0a2922cc5
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-02-27 21:40:44 +0330

    another project added!

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,16 +4,26 @@ import "../styles/styles.css";
 const Projects = () => {
   return (
     <div className="projectsContainer">
+      <h1>
+          My <span>Projects!</span>
+        </h1>
       <div className="projects">
+      <div>
+          <h1>1.Portfolio</h1>
+          <p>Checkout my portfolio page's codes!</p>
+          <a href="https://github.com/EhsanTheCoder98/portfolio.git" target="_blank">
+            Click to see the project
+          </a>
+        </div>
         <div>
-          <h1>1.Verificattion form</h1>
+          <h1>2.Verificattion form</h1>
           <p>It's a login/signup form with validations i wrote Using JS</p>
           <a href="https://github.com/EhsanTheCoder98/Login-form-with-validations.git" target="_blank">
             Click to see the project
           </a>
         </div>
         <div>
-          <h1>2.Contacts App</h1>
+          <h1>3.Contacts App</h1>
           <p>
             I developed a ReactJS application resembling a digital phonebook,
             complete with data validation features to ensure the accuracy of
@@ -25,7 +35,7 @@ const Projects = () => {
           </a>
         </div>
         <div>
-          <h1>3.Book Library</h1>
+          <h1>4.Book Library</h1>
           <p>
             I have developed a basic book library application utilizing a
             JavaScript document to store book information, with an added feature
@@ -36,7 +46,7 @@ const Projects = () => {
           </a>
         </div>
         <div>
-          <h1>4.Shop</h1>
+          <h1>5.Shop</h1>
           <p>
             The shopping app includes a login page, a products page, a product
             details page, and features filtering and searching capabilities. It
